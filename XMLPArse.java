2232c0c1d0e5cf8375048de3d863a08ff52682f7
ascanu <singh.anupam0@gmail.com>
2020-07-17 17:15:56 +0530

    Add files via upload

--- XMLPArse.java
+++ XMLPArse.java
@@ -20,6 +20,24 @@ public class XMLPArse {
 	public static void main(String[] args) throws SAXException, Exception {
 		File sourceFile=new File("./src/main/java/com/etsy/qa/testdata/source/source.xml");
 		File targetFile=new File("./src/main/java/com/etsy/qa/testdata/Destinatioin/desti.xml");
+		
+		
+		DiffElements processXMLDiffDocument = processXMLDiffDocument(sourceFile,targetFile);
+		
+		List<XMLDiffMap> addedElements = processXMLDiffDocument.getAddedElements();
+		List<XMLDiffMap> deletedElements = processXMLDiffDocument.getDeletedElements();
+		
+		for(XMLDiffMap element : addedElements) {
+			System.out.println(element.getPath());
+			System.out.println(element.getValue());
+		}
+	}
+	
+	
+	
+	
+	private static DiffElements processXMLDiffDocument(File sourceFile, File targetFile) throws ParserConfigurationException, SAXException, IOException {
+		// TODO Auto-generated method stub
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document sourcedoc = dBuilder.parse(sourceFile);
@@ -39,13 +57,14 @@ public class XMLPArse {
 		
 		
 		//System.out.println("targetlist : " + targetList);
-		 listDifference(sourceList,targetList);
+		 return listDifference(sourceList,targetList);
+		 
 		
 	}
-	
-	
-	
-	
+
+
+
+
 	private static void printNode(NodeList nodelist, Map<String, String> targetList) {
 		String Keys=null;
 		Set<Integer> NodeTypesSet = new HashSet<Integer>();
@@ -64,9 +83,9 @@ public class XMLPArse {
 					 * System.out.println("###################################");
 					 * System.out.println("Base URI : " + tempNode.getParentNode().getNodeName());
 					 */
-					 System.out.println("Base URI : " + getBaseNode(tempNode));
+					// System.out.println("Base URI : " + getBaseNode(tempNode));
 				//	 System.out.println("Base URI : " + getBaseNode2(tempNode));
-					 System.out.println("Text value : " + tempString);
+				//	 System.out.println("Text value : " + tempString);
 					/*
 					 * System.out.println("###################################");
 					 */	
@@ -110,17 +129,94 @@ public class XMLPArse {
 		}
 		
 	}
-	public static void listDifference(Map<String, String> sourceList, Map<String, String> targetList) {
+	public static DiffElements listDifference(Map<String, String> sourceList, Map<String, String> targetList) {
+		
+		final Map<String,String> dupSourceMap = new HashMap<String,String>();
+		final Map<String,String> dupTargetMap = new HashMap<String,String>();
+		
+		sourceList.forEach((k,v) -> dupSourceMap.put(v,k));
+		targetList.forEach((k,v) -> dupTargetMap.put(v,k));
 		
 		
-        System.out.print("Added"+CollectionUtils.removeAll(targetList,sourceList)); 
-		System.out.print("<----------------------------------------.>"); 
-		System.out.print("deleted"+CollectionUtils.removeAll(sourceList,targetList));
-		  
-		
-		
+        List<String> addValues = (List<String>) CollectionUtils.removeAll(targetList.values(),sourceList.values());
+        List<String> deletedValues = (List<String>) CollectionUtils.removeAll(sourceList.values(),targetList.values());
 	
+        List<XMLDiffMap> addedElements = new ArrayList<XMLDiffMap>();
+        List<XMLDiffMap> deletedElements = new ArrayList<XMLDiffMap>();
+        
+        System.out.println("ADDED ELEMENTS");
+        addValues.stream().forEach(element -> {
+        	System.out.println();
+        	String key = dupTargetMap.get(element);
+        	String value = element;
+        	addedElements.add(new XMLDiffMap(key, value));
+        	System.out.println("Path is : " + key);
+        	System.out.println("Value is : " + value);
+        });
 		
+        System.out.println("DELETED ELEMENTS");
+        deletedValues.stream().forEach(element -> {
+        	System.out.println();
+        	String key = dupSourceMap.get(element);
+        	String value = element;
+        	deletedElements.add(new XMLDiffMap(key,value));
+        	System.out.println("Path is : " + key);
+        	System.out.println("Value is : " + value);
+        });
+		
+        
+        return new DiffElements(addedElements,deletedElements);
 	}
 
+}
+
+
+class DiffElements{
+	
+	private List<XMLDiffMap> deletedElements;
+	private List<XMLDiffMap> addedElements;
+	
+	public DiffElements() {
+		// TODO Auto-generated constructor stub
 	}
+	
+	public DiffElements(List<XMLDiffMap> addedElements,  List<XMLDiffMap> deletedElements) {
+		// TODO Auto-generated constructor stub
+		this.addedElements = addedElements;
+		this.deletedElements = deletedElements;
+	}
+	
+	public List<XMLDiffMap> getAddedElements() {
+		return addedElements;
+	}
+	
+	public List<XMLDiffMap> getDeletedElements() {
+		return deletedElements;
+	}
+	
+	
+	
+}
+
+class XMLDiffMap {
+	
+	private String path;
+	private String value;
+	
+	
+	XMLDiffMap(String path,String value){
+		this.path = path;
+		this.value = value;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	
+	
+}
